29356a79defa5de674fef1dc5ac4f815560e9634
Aron de Castro <aronxgames@yahoo.com.br>
2015-06-22 12:06:48 -0300

    Adiciona um form mixim

---
+++ ajax_upload/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .utils import move_file
+
+
+class FormUploadMixin(forms.ModelForm):
+    fields = []
+    destination = "/"
+
+    def move_file(self, field):
+        data = self.data[field]
+        url_dst = move_file(data, self.destination)
+        self.cleaned_data[field] = url_dst
+    
+    def is_valid(self):
+        valid = super(
+            TeacherForm, 
+            self).is_valid()
+
+        if(valid):
+            for(field in fileds):
+                self.move_file(field)
+
+        return valid
